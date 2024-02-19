22c7e3697871ce9e50ce07f5d030c1a8296e8c82
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-02-19 18:24:05 +0100

    feat (AppHeader) /AppNav /AppNavBar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <script>
-  export default{
+import AppHeader from './components/1-header/AppHeader.vue'
+import AppMain from './components/2-main/AppMain.vue'
+import AppFooter from './components/3-footer/AppFooter.vue'
 
+  export default{
+    components: {
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
   }
 </script>
 
 <template>
-  
+  <header>
+    <AppHeader/>
+  </header>
+
+  <main>
+    <AppMain/>
+  </main>
+
+  <footer>
+    <AppFooter/>
+  </footer>
 </template>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+header{
+  background-image: url('./assets/img/header-bg.png');
+}
 </style>
